e4050e2b23736ee4e19eefc263936240f639e911
ivanstiment <ivansostiment@gmail.com>
2025-05-22 20:51:18 +0200

    table news part 1 split 8 kb file

--- scripts/django_postbuild.sh
+++ scripts/django_postbuild.sh
@@ -1,44 +1,45 @@
 #!/bin/bash
 
 echo "🔧 Django Post-Build Script"
-echo "📍 Working directory: $(pwd)"
-echo "🕒 Date: $(date)"
-echo "🐍 Python version: $(python --version)"
+# echo "📍 Working directory: $(pwd)"
+# echo "🕒 Date: $(date)"
+# echo "🐍 Python version: $(python --version)"
 
-# ⚙️ Verificación de settings
-DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-news_trader.settings}
-export DJANGO_SETTINGS_MODULE
-echo -e "\n⚙️ Usando configuración: $DJANGO_SETTINGS_MODULE"
+# # ⚙️ Verificación de settings
+# DJANGO_SETTINGS_MODULE=${DJANGO_SETTINGS_MODULE:-news_trader.settings}
+# export DJANGO_SETTINGS_MODULE
+# echo -e "\n⚙️ Usando configuración: $DJANGO_SETTINGS_MODULE"
 
-# 🔗 Verificación de base de datos
-echo -e "\n🔎 Probando conexión a la base de datos..."
-python -c "
-import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', '$DJANGO_SETTINGS_MODULE')
-import django
-django.setup()
-from django.db import connection
-try:
-    with connection.cursor() as cursor:
-        cursor.execute('SELECT 1')
-        print('✅ Conexión a la base de datos exitosa')
-except Exception as e:
-    print(f'❌ ERROR de conexión a la base de datos: {e}')
-"
+# # 🔗 Verificación de base de datos
+# echo -e "\n🔎 Probando conexión a la base de datos..."
+# python -c "
+# import os
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', '$DJANGO_SETTINGS_MODULE')
+# import django
+# django.setup()
+# from django.db import connection
+# try:
+#     with connection.cursor() as cursor:
+#         cursor.execute('SELECT 1')
+#         print('✅ Conexión a la base de datos exitosa')
+# except Exception as e:
+#     print(f'❌ ERROR de conexión a la base de datos: {e}')
+# "
 
 # 🛠️ Aplicar migraciones
-echo -e "\n📦 Aplicando migraciones..."
-python manage.py migrate --noinput || { echo '❌ Fallo al aplicar migraciones'; exit 1; }
+# echo -e "\n📦 Aplicando migraciones..."
+# python manage.py migrate --noinput || { echo '❌ Fallo al aplicar migraciones'; exit 1; }
 
 # 🛠️ Volcar los datos de la base de datos sqlite a la base de datos postgres
 # echo -e "\n📦 Aplicando los datos de la base de datos sqlite auth_user... "
 # python manage.py loaddata fixtures/auth_user.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
 
-echo -e "\n📦 Aplicando los datos de la base de datos sqlite admin_logentry..."
-python manage.py loaddata fixtures/admin_logentry.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
+# echo -e "\n📦 Aplicando los datos de la base de datos sqlite admin_logentry..."
+# python manage.py loaddata fixtures/admin_logentry.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
+# -----------------
 
-# echo -e "\n📦 Aplicando los datos de la base de datos sqlite news_new_001..."
-# python manage.py loaddata fixtures/news_new_001.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
+echo -e "\n📦 Aplicando los datos de la base de datos sqlite news_new_001..."
+python manage.py loaddata fixtures/news_new_001.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
 
 # echo -e "\n📦 Aplicando los datos de la base de datos sqlite news_new_002..."
 # python manage.py loaddata fixtures/news_new_002.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
