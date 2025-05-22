898c8320033e526bf03d7c3ef4547ab410e30d1f
ivanstiment <ivansostiment@gmail.com>
2025-05-22 19:27:10 +0200

    auth_user table content

--- scripts/django_postbuild.sh
+++ scripts/django_postbuild.sh
@@ -31,14 +31,14 @@ echo -e "\n📦 Aplicando migraciones..."
 python manage.py migrate --noinput || { echo '❌ Fallo al aplicar migraciones'; exit 1; }
 
 # 🛠️ Volcar los datos de la base de datos sqlite a la base de datos postgres
-# echo -e "\n📦 Aplicando los datos de la base de datos sqlite auth_user... "
-# python manage.py loaddata fixtures/auth_user.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
+echo -e "\n📦 Aplicando los datos de la base de datos sqlite auth_user... "
+python manage.py loaddata fixtures/auth_user.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
 
 # echo -e "\n📦 Aplicando los datos de la base de datos sqlite admin_logentry..."
 # python manage.py loaddata fixtures/admin_logentry.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
 
-echo -e "\n📦 Aplicando los datos de la base de datos sqlite news_new_001..."
-python manage.py loaddata fixtures/news_new_001.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
+# echo -e "\n📦 Aplicando los datos de la base de datos sqlite news_new_001..."
+# python manage.py loaddata fixtures/news_new_001.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
 
 # echo -e "\n📦 Aplicando los datos de la base de datos sqlite news_new_002..."
 # python manage.py loaddata fixtures/news_new_002.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
@@ -58,8 +58,8 @@ python manage.py loaddata fixtures/news_new_001.json || { echo '❌ Fallo al car
 # echo -e "\n📦 Aplicando los datos de la base de datos sqlite news_newsanalysis..."
 # python manage.py loaddata fixtures/news_newsanalysis.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
 
-echo -e "\n📦 Aplicando los datos de la base de datos sqlite news_stock..."
-python manage.py loaddata fixtures/news_stock.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
+# echo -e "\n📦 Aplicando los datos de la base de datos sqlite news_stock..."
+# python manage.py loaddata fixtures/news_stock.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
 
 # echo -e "\n📦 Aplicando los datos de la base de datos sqlite sessions_session..."
 # python manage.py loaddata fixtures/sessions_session.json || { echo '❌ Fallo al cargar los datos de la base de datos sqlite'; exit 1; }
