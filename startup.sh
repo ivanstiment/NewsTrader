012961fb4c138b6602f9439115df0fec52317f95
ivanstiment <ivansostiment@gmail.com>
2025-05-24 01:22:59 +0200

    modify celey launch script

--- startup.sh
+++ startup.sh
@@ -10,4 +10,12 @@ echo "Arrancando Gunicorn y Celery..."
 gunicorn news_trader.wsgi:application --bind=0.0.0.0:8000 &
 
 # Lanza Celery worker (usa el venv actual, Oryx ya lo activó)
-celery -A news_trader worker --loglevel=INFO
+celery -A news_trader worker \
+    --loglevel=info \
+    --events \
+    --concurrency=4 \
+    --max-tasks-per-child=1000 \
+    --time-limit=300 \
+    --soft-time-limit=240 \
+    --prefetch-multiplier=1
+    # --loglevel=warning \
