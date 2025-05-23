9964c2b894cd908edb85bbb2ae5f853f2ee4c71b
ivanstiment <ivansostiment@gmail.com>
2025-05-23 20:43:35 +0200

    commit for build and deploy

--- App_Data/jobs/continuous/celeryworker/run.sh
+++ App_Data/jobs/continuous/celeryworker/run.sh
@@ -2,5 +2,3 @@
 echo "Activando entorno virtual y ejecutando Celery..."
 source /home/site/wwwroot/antenv/bin/activate
 exec celery -A news_trader worker --loglevel=info
-
-# /home/site/wwwroot/antenv/bin/celery -A news_trader worker --loglevel=info
