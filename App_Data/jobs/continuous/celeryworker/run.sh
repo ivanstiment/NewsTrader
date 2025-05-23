8f409f092b721fd9cfbe5524d569046b900ebc99
ivanstiment <ivansostiment@gmail.com>
2025-05-23 20:34:58 +0200

    try Create a wroker in deploy

---
+++ App_Data/jobs/continuous/celeryworker/run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Activando entorno virtual y ejecutando Celery..."
+source /home/site/wwwroot/antenv/bin/activate
+exec celery -A news_trader worker --loglevel=info
+
+# /home/site/wwwroot/antenv/bin/celery -A news_trader worker --loglevel=info
